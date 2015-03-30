df9142050db7a5ae9114445ab804c61012b2cf6f
paugier <pierre.augier@ens-lyon.org>
2015-03-31 00:39:46 +0200

    Add a gui file

---
+++ fluidlab/util/__init__.py
@@ -0,0 +1,15 @@
+"""
+Utilities for the lab (:mod:`fluidlab.util`)
+============================================
+
+.. currentmodule:: fluidlab
+
+Provides
+
+.. autosummary::
+   :toctree:
+
+   gui
+
+
+"""
