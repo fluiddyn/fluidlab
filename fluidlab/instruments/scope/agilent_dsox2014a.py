b78b718c444caaf135a3da919358d37884e5dba5
paugier <pierre.augier@ens-lyon.org>
2015-08-02 21:51:11 +0200

    Improve agilent dsox2014a.

--- fluidlab/instruments/scope/agilent_dsox2014a.py
+++ fluidlab/instruments/scope/agilent_dsox2014a.py
@@ -86,7 +86,7 @@ class AgilentDSOX2014a(IEC60488, Trigger, ObjectIdentification,
         return time, data
 
 
-AgilentDSOX2014a._build_class_with_features([
+features = [
     WriteCommand(
         'autoscale', doc='Autoscale the oscilloscope.',
         command_str=':AUTOscale'),
@@ -94,37 +94,42 @@ AgilentDSOX2014a._build_class_with_features([
         'nb_points', doc='The number of points returned.',
         command_set=':WAVeform:POINts'),
     FloatValue(
-        'channel1_probe_attenuation',
-        doc="""The probe attenuation ratio.""",
-        command_set=':CHANnel1:PROBe'),
-    FloatValue(
-        'channel1_range',
-        doc="""The vertical full-scale range value (in volt).""",
-        command_set=':CHANnel1:range'),
-    FloatValue(
-        'channel1_scale',
-        doc="""The number of units per division of the channel.""",
-        command_set=':CHANnel1:SCALe'),
-    StringValue(
-        'channel1_coupling',
-        doc="""The type of input coupling for the channel.
-
-        It can be set to "AC" or "DC".""",
-        command_set=':CHANnel1:COUPling',
-        valid_values=['ac', 'dc']),
-    BoolValue(
-        'channel1_display',
-        doc="""A boolean setting the display of the channel.""",
-        command_set=':CHANnel1:DISPlay'),
-    FloatValue(
         'timebase_range',
         doc="""The time for 10 division in seconds.""",
         command_set=':TIMebase:RANGe'),
     FloatValue(
         'trigger_level',
         doc="""The trigger level voltage for the active trigger source.""",
-        command_set=':TRIGger:LEVel')
-])
+        command_set=':TRIGger:LEVel')]
+
+for channel in range(1, 3):
+    features += [
+        FloatValue(
+            'channel{}_probe_attenuation'.format(channel),
+            doc="""The probe attenuation ratio.""",
+            command_set=':CHANnel{}:PROBe'.format(channel)),
+        FloatValue(
+            'channel{}_range'.format(channel),
+            doc="""The vertical full-scale range value (in volt).""",
+            command_set=':CHANnel{}:range'.format(channel)),
+        FloatValue(
+            'channel{}_scale'.format(channel),
+            doc="""The number of units per division of the channel.""",
+            command_set=':CHANnel{}:SCALe'.format(channel)),
+        StringValue(
+            'channel{}_coupling'.format(channel),
+            doc="""The type of input coupling for the channel.
+
+It can be set to "AC" or "DC".""",
+            command_set=':CHANnel{}:COUPling'.format(channel),
+            valid_values=['ac', 'dc']),
+        BoolValue(
+            'channel{}_display'.format(channel),
+            doc="""A boolean setting the display of the channel.""",
+            command_set=':CHANnel{}:DISPlay'.format(channel))]
+
+
+AgilentDSOX2014a._build_class_with_features(features)
 
 
 if __name__ == '__main__':
