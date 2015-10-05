e0d778f026051a377b7d571b43a850511cfc48b0
paugier <pierre.augier@ens-lyon.org>
2015-10-05 13:56:55 +0200

    Communicate with the IPS 2303S.

--- fluidlab/instruments/powersupply/isotech_ips2303s.py
+++ fluidlab/instruments/powersupply/isotech_ips2303s.py
@@ -12,18 +12,16 @@ from fluidlab.instruments.drivers import VISADriver
 from fluidlab.instruments.features import (
     QueryCommand, FloatValue)
 
+
 class IsoTechIPS2303S(VISADriver):
     """Driver for the power supply IPS 2303S.
 
     """
 
 
-
 def _parse_status_code(code):
 
     return code
-    
-    
 
 
 features = [
@@ -34,7 +32,7 @@ features = [
         'query_status',
         'Query status\n\n'
         'Return a dictionary containing information of the device.',
-        command_str='STATUS?'
+        command_str='STATUS?',
         parse_result=_parse_status_code)]
 
 for channel in [1, 2]:
@@ -57,4 +55,30 @@ for channel in [1, 2]:
             'VOUT{}'.format(channel)),
     ])
 
-HP34401a._build_class_with_features(features)
+
+IsoTechIPS2303S._build_class_with_features(features)
+
+
+if __name__ == '__main__':
+
+    import serial
+    from serial.tools.list_ports import comports
+
+    port = None
+    for info_port in comports():
+        if '0403:6001' in info_port[2]:
+            port = info_port[0]
+
+    if port is None:
+        raise ValueError('The device does not seem to be plugged.')
+
+    sp = serial.Serial(
+        port=port,
+        baudrate=9600,  # warning: does work for higher baudrate!
+        timeout=1, bytesize=8, parity='N', stopbits=1)
+
+    sp.readline()
+
+    sp.write('*IDN?\r\n'.encode())
+
+    print(sp.readline())
