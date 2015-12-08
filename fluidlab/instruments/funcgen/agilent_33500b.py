d9151693b7364e310dc24358554652bd7edbd777
paugier <pierre.augier@ens-lyon.org>
2015-12-08 10:07:37 +0100

    Add a module for the Agilent 33500B function generator.

---
+++ fluidlab/instruments/funcgen/agilent_33500b.py
@@ -0,0 +1,16 @@
+"""Agilent 33500b
+=================
+
+.. autoclass:: Agilent33500b
+   :members:
+   :private-members:
+
+"""
+
+
+from fluidlab.instruments.funcgen.agilent_33220a import Agilent33220a
+
+
+class Agilent33500b(Agilent33220a):
+    """Minimal implementation of a driver for the Agilent 33500B."""
+    pass
