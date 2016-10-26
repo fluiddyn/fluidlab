3dc139a914a4df4f63a636a681759dfb55c0e375
Julien Salort <julien.salort@ens-lyon.fr>
2016-10-26 20:36:53 +0200

    Add HP 6653A power supply

--- fluidlab/instruments/powersupply/__init__.py
+++ fluidlab/instruments/powersupply/__init__.py
@@ -11,4 +11,4 @@ Provides:
 
 """
 
-__all__ = ('agilent_6030a', 'isotech_ips2303s', 'tdk_lambda', 'tti_cpx400dp')
+__all__ = ('agilent_6030a', 'isotech_ips2303s', 'tdk_lambda', 'tti_cpx400dp', 'hp_6653a')
