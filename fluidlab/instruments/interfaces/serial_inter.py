c98f475f8ee14078a640c69d630e5f9565707e73
jsalort <none@none>
2018-01-08 18:55:14 +0100

    Add support for Thorlabs Optical meter

--- fluidlab/instruments/interfaces/serial_inter.py
+++ fluidlab/instruments/interfaces/serial_inter.py
@@ -10,6 +10,7 @@ Provides:
 """
 
 import serial
+import io
 
 from time import sleep
 
@@ -18,7 +19,11 @@ from fluidlab.instruments.interfaces import QueryInterface
 class SerialInterface(QueryInterface):
     def __init__(self, port, baudrate=9600, bytesize=8,
                  parity='N', stopbits=1, timeout=1, xonxoff=False,
-                 rtscts=False, dsrdtr=False):
+                 rtscts=False, dsrdtr=False, eol=None, multilines=False):
+        """
+        if eol is not None, the serial port is wrapped into TextIOWrapper to
+        allow readline to wait for an eol which is not \n
+        """
 
         # open serial port
         sp = serial.Serial(
@@ -26,13 +31,38 @@ class SerialInterface(QueryInterface):
             parity=parity, stopbits=stopbits, timeout=timeout,
             xonxoff=xonxoff, rtscts=rtscts, dsrdtr=dsrdtr)
         self._lowlevel = self.serial_port = sp
-        self.write = sp.write
-        self.readline = sp.readline
         self.close = sp.close
-
+        self.eol = eol
+        self.multilines = multilines
+        if eol is not None:
+            self.ser_io = io.TextIOWrapper(io.BufferedRWPair(sp, sp, 1),
+                                           newline = eol,
+                                           line_buffering = True)
+            
+    def write(self, *args):
+        if self.eol is not None:
+            return self.ser_io.write(*args)
+        return self.serial_port.write(*args)
+        
+    def readline(self, *args):
+        if self.eol is not None:
+            return self.ser_io.readline(*args)
+        return self.serial_port.readline(*args)
+    
+    def readlines(self, *args):
+        if self.eol is not None:
+            return self.ser_io.readlines(*args)
+        return self.serial_port.readlines(*args)
+        
     def read(self):
-        result = self.serial_port.readline()
-        return b'\n'.join(result.splitlines())
+        if self.multilines:
+            result = self.readlines()
+            return '\n'.join(result)
+        else:
+            result = self.readline()
+            if isinstance(result, str):
+                return result
+            return b'\n'.join(result.splitlines())
 
     def query(self, command, latence=0.):
         self.write(command)
--- fluidlab/instruments/multimeter/__init__.py
+++ fluidlab/instruments/multimeter/__init__.py
@@ -11,4 +11,4 @@ Provides:
 
 """
 
-__all__ = ["hp_34401a"]
+__all__ = ["hp_34401a", "thorlabs_s110"]
