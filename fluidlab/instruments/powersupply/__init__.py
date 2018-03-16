5f0dbd42f290af670a59cd5bef4748ba49e4b3ff
Julien Salort <julien.salort@ens-lyon.fr>
2018-03-16 16:54:11 +0100

    Add support for Xantrex XDC-300 power supply

--- fluidlab/instruments/powersupply/__init__.py
+++ fluidlab/instruments/powersupply/__init__.py
@@ -12,8 +12,9 @@ Provides:
    tdk_lambda
    tti_cpx400dp
    hp_6653a
+   xantrex_xdc300
 
 """
 
 __all__ = ('agilent_6030a', 'isotech_ips2303s', 'tdk_lambda', 'tti_cpx400dp',
-           'hp_6653a')
+           'hp_6653a', 'xantrex_xdc300')
