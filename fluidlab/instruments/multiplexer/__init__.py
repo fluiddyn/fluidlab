39003da5a340b61fb497bd3b1c6b5e904c117238
Julien Salort <julien.salort@ens-lyon.fr>
2019-03-18 17:46:14 +0100

    Add Cryocon 24c in the __all__ variable of multiplexer.py

--- fluidlab/instruments/multiplexer/__init__.py
+++ fluidlab/instruments/multiplexer/__init__.py
@@ -10,6 +10,8 @@ Provides:
    keithley_2700
    keithley_705
    lakeshore_224
+   cryocon_24c
 
 """
-__all__ = ["agilent_34970a", "keithley_2700", "lakeshore_224", "keithley_705"]
+__all__ = ["agilent_34970a", "keithley_2700", "lakeshore_224", "keithley_705",
+           "cryocon_24c"]
