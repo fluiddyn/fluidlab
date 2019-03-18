745312db4f2b646d52f0284bfd942b76a723d0d8
Julien Salort <julien.salort@ens-lyon.fr>
2019-03-18 16:58:44 +0100

    Add implementation for Cryocon 24C temperature controller

---
+++ fluidlab/instruments/multiplexer/cryocon_24c.py
@@ -0,0 +1,29 @@
+"""
+
+Driver for the CryoCon 24C Temperature controller
+
+"""
+
+__all__ = ["Cryocon24c"]
+
+from fluidlab.instruments.iec60488 import IEC60488
+from fluidlab.instruments.features import FloatValue
+
+class Cryocon24c(IEC60488):
+    default_port = 5000
+    
+
+features = [
+    FloatValue("temperature",
+        doc="Reads temperature",
+        command_get="input? {channel:}",
+        channel_argument=True,
+    ),
+]
+
+Cryocon24c._build_class_with_features(features)
+    
+if __name__ == '__main__':
+    with Cryocon24c('192.168.0.2') as cc:
+        Ta = cc.temperature.get('A')
+        print('Ta =', Ta)
